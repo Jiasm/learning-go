b78e0470405a11aff516e12f3346ddd80da958fd
jarvis <jiashunming@outlook.com>
2020-04-27 23:00:15 +0800

    feat: add link list remove after

---
+++ src/algorithms/ch1/link-list-remove-after/link-list-remove-after.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+func main() {
+	linkList := &LinkList{ nil }
+
+	linkList.add(1)
+	linkList.add(2)
+	linkList.add(3)
+	linkList.add(4)
+	linkList.add(5)
+
+	linkList.removeAfter(3)
+	fmt.Println(linkList.find(3))
+}
+
+type LinkListItem struct {
+	value int
+	next 	*LinkListItem
+}
+
+type LinkList struct {
+	first *LinkListItem
+}
+
+func (l *LinkList) add(val int) {
+	current := &LinkListItem{ val, nil }
+	if l.first == nil {
+		l.first = current
+	} else {
+		l.last().next = current
+	}
+}
+
+func (l *LinkList) last() *LinkListItem {
+	if l.first == nil {
+		return nil
+	} else {
+		current := l.first
+		for current.next != nil {
+			current = current.next
+		}
+
+		return current
+	}
+}
+
+func (l *LinkList) find(val int) *LinkListItem {
+	if l.first == nil {
+		return nil
+	} else {
+		current := l.first
+		for ;; {
+			if current.value == val {
+				return current
+			}
+
+			if current.next != nil {
+				current = current.next
+			} else {
+				return nil
+			}
+		}
+	}
+}
+
+func (l *LinkList) removeAfter(val int) {
+	target := l.find(val)
+
+	if target != nil {
+		target.next = nil
+	}
+}
